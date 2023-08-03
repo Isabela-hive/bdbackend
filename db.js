72148b748d71cfce5cda1937a87812049250c859
Your Name <you@example.com>
2023-08-03 16:26:16 +0300

    adding env

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
+require('dotenv').config();
 
 var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '@isabellah',
-  database: 'bunyala',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 con.connect(function (err) {
