708537025ba3bda25d188113110c1708838545fc
Your Name <you@example.com>
2023-08-03 14:58:35 +0300

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 2000;
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // This is for connecting to the DB
-require("./db");
+require('./db');
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/", require("./routes/home"));
-app.use("/st", require("./routes/static"));
-app.use("/mainmedia", require("./routes/mainmedia"));
+app.use('/', require('./routes/home'));
+app.use('/st', require('./routes/static'));
+app.use('/mainmedia', require('./routes/mainmedia'));
 
 app.listen(port, () => {
   console.log(`Budalangi Backend on port ${port}`);
